d882dce388a868ba5bd701001d89818742de19f3
Ziel Adam <ziela@wit.edu>
2016-11-29 20:10:46 -0500

    Alright David, I'm done playing games,,,,, I have won

--- app/src/main/java/com/example/ziela/gaitsynthesizer/ConfigurationActivity.java
+++ app/src/main/java/com/example/ziela/gaitsynthesizer/ConfigurationActivity.java
@@ -11,10 +11,12 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.SeekBar;
 import android.widget.TextView;
+import android.view.View.OnTouchListener;
 
 /**
  * This class will be used for setting up the pedometer.
@@ -22,82 +24,84 @@ import android.widget.TextView;
  * This means steps are not tracked until its calibrated, so I want to use this class to do the calibration
  * phase and THEN pass it to the music synthesis.
  */
-public class ConfigurationActivity extends AppCompatActivity implements SensorEventListener {
-
+public class ConfigurationActivity extends AppCompatActivity
+        implements SensorEventListener, OnTouchListener
+{
     WebView webView;
     private TextView textView, noteTextView;
+
     private SensorManager mSensorManager;
     private Sensor mStepDetectorSensor;
-    private int count = 0;
-
-    public static final String STARTING_NOTE_STRING = "STARTING_NOTE_STRING";
-    private SeekBar noteSelectBar;
-    int startNote;
-
-
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
-        WebView webView = (WebView)
-                findViewById(R.id.web_view);
-        webView.setWebViewClient(new WebViewClient());//stay in app
+
+        // display loading gif in browser -- let's get off the web -- we're going offline
+        WebView webView = (WebView) findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://thomas.vanhoutte.be/miniblog/wp-content/uploads/light_blue_material_design_loading.gif");
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setUseWideViewPort(true);
-        noteTextView = (TextView) findViewById(R.id.startNoteTextView);
-        noteSelectBar = (SeekBar) findViewById(R.id.seekBar);
-        startNote = noteSelectBar.getProgress(); //get his initial val
-        noteTextView.setText("The starting note will be " + (startNote+45)); //set initial text
-        //scale him up by 1 b/c ppl are dumb and dont know counts start at 0
 
-        noteSelectBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int seekBarValue, boolean fromUser) {
-                startNote = seekBarValue;
-                noteTextView.setText("The starting note will be " + (startNote+45)); //update the msg to user
-                ////scale him up by 1 b/c ppl are dumb and dont know counts start at 0
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
-        });
-
+        // get XML handles
+        noteTextView = (TextView) findViewById(R.id.inputNoteTextView);
         textView = (TextView) findViewById(R.id.stepsTakenText);
 
+        // again, what are these doing
         mSensorManager = (SensorManager) getSystemService(this.SENSOR_SERVICE);
         mStepDetectorSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
 
+        // get button handle and set listener
+        View v = findViewById(R.id.bypassButton);
+        if (v != null)
+            v.setOnTouchListener(this);
     }
 
+    /**
+     * Called every time a sensor event occurs.
+     * If the event is a STEP_DETECT, launch into MainActivity
+     *
+     * @param event
+     */
+    public void onSensorChanged(SensorEvent event)
+    {
+        Sensor sensor = event.sensor;
+
+        // once we detect step, calibration is finished, so move to main activity
+        if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR)
+        {
+            Intent main = new Intent(ConfigurationActivity.this, MainActivity.class);
+
+            ConfigurationActivity.this.startActivity(main);
+
+            finish();
+        }
+    }
+
+    @Override
+    /*
+     * This method detects buttons presses,
+     * and bypasses the step detector calibration
+     */
+    public boolean onTouch(View v, MotionEvent event)
+    {
+        if (event.getAction() == MotionEvent.ACTION_DOWN)
+        {
+            Intent main = new Intent(ConfigurationActivity.this, MainActivity.class);
+
+            ConfigurationActivity.this.startActivity(main);
+
+            finish();
+        }
+
+        return false;
+    }
 
     public void onAccuracyChanged(final Sensor sensor, int accuracy){
         // shouldn't be called, need to implement SensorEventListener
-    }
-    public void onSensorChanged(SensorEvent event) {
-        Sensor sensor = event.sensor;
-        float[] values = event.values;
-        int value = -1;
-
-        if (values.length > 0) {
-            value = (int) values[0];
-        }
-
-        if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
-            // For test only. Only allowed value is 1.0 i.e. for step taken
-            this.count++;
-            textView.setText("Step Detector Detected : " + count);
-            Intent synthesis = new Intent(ConfigurationActivity.this, MainActivity.class);
-            synthesis.putExtra(STARTING_NOTE_STRING, (startNote + 45));
-            ConfigurationActivity.this.startActivity(synthesis);
-            finish();
-        }
     }
 
     protected void onResume() {
